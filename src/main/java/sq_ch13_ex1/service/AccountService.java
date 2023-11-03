d795cec2f7624338dce9c90aef84cb29f6db3e63
osparking <jbpark03@gmail.com>
2023-11-03 21:19:23 +0900

    모든 계좌 정보 읽는 서비스 메소드

--- src/main/java/sq_ch13_ex1/service/AccountService.java
+++ src/main/java/sq_ch13_ex1/service/AccountService.java
@@ -1,11 +1,13 @@
 package sq_ch13_ex1.service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
+import sq_ch13_ex1.model.Account;
 import sq_ch13_ex1.repository.AccountRepository;
 
 @Service
@@ -26,6 +28,10 @@ public class AccountService {
 		// 두 계정의 금액을 변경한다.
 		repository.changeAmount(acntSrc.getId(), amtSrc);
 		repository.changeAmount(acntDst.getId(), amtDst);
+	}
+	
+	public List<Account> getAllAccounts() {
+		return repository.allAccounts();
 	}
 
 }
