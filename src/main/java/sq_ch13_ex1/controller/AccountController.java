7c12c9969fad6ca3f4a6bd1a9000fa7bc1c906cb
osparking <jbpark03@gmail.com>
2023-11-03 21:46:23 +0900

    모든 계좌 읽는 액션 메소드

--- src/main/java/sq_ch13_ex1/controller/AccountController.java
+++ src/main/java/sq_ch13_ex1/controller/AccountController.java
@@ -1,11 +1,15 @@
 package sq_ch13_ex1.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import sq_ch13_ex1.TransferRequest;
+import sq_ch13_ex1.model.Account;
 import sq_ch13_ex1.service.AccountService;
 
 @RestController
@@ -22,6 +26,11 @@ public class AccountController {
 				request.getSenderAccountId(), 
 				request.getReceiverAccountId(),
 				request.getAmount());
+	}
+	
+	@GetMapping("/accounts")
+	public List<Account> getAllAccounts() {
+		return service.getAllAccounts();
 	}
 
 }
